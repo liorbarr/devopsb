4d2bbfa2f457b6db85a96552fdf5cfe1e2594393
Your Name <you@example.com>
2025-09-11 13:41:46 +0300

    testnew

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.get("/")
 def home():
-    return "Hello from Docker!"
+    return "מה קורה צדיק!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
